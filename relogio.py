abbe03ba305b7e3c319e0b44c5396d0faa573126
gabrielfernandessilva <gabriel.fernandes.silva@ccc.ufcg.edu.br>
2017-04-19 18:50:41 -0300

    organizando relogio

--- relogio.py
+++ relogio.py
@@ -6,20 +6,25 @@ Implementacao simples de relogio
 @Author Gabriel Fernandes
 '''
 
+import sys
 import tkinter
 from time import strftime
 
-tk = tkinter.Tk()
-tk.title('Meu Relogio')
-janela = tkinter.Label(tk)
-janela['font'] = 'Helvetica 80 bold'
-janela['text'] = strftime('%H:%M:%S')
+def main(args):
+	tk = tkinter.Tk()
+	tk.title('Meu Relogio')
+	global janela
+	janela = tkinter.Label(tk)
+	janela['font'] = 'Helvetica 80 bold'
+	janela['text'] = strftime('%H:%M:%S')
+
+	janela.pack()
+	atualiza_tempo()
+	janela.mainloop()
 
 def atualiza_tempo():
 	janela['text'] = strftime('%H:%M:%S')
 	janela.after(10, atualiza_tempo)
 
 if __name__=='__main__':
-	janela.pack()
-	atualiza_tempo()
-	janela.mainloop()
+	main(sys.argv)
