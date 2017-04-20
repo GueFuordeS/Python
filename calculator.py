4b05b42f70b77e34c2506002367c1984afcc75b3
gabrielfernandessilva <gabriel.fernandes.silva@ccc.ufcg.edu.br>
2017-04-20 18:04:19 -0300

    ultimos estagios da calculadora

--- calculator.py
+++ calculator.py
@@ -7,12 +7,51 @@ calculadora
 @Author Gabriel Fernandes
 '''
 
+import sys
 from tkinter import *
 from functools import partial
 
+screen_text = ''
+contents_table = ''
+
 def update_screen(button):
+    global screen_text
     screen_text += button
     screen['text'] = screen_text
+    
+def clean_screen():
+    global screen_text
+    screen_text = ''
+    screen['text'] = '0'
+
+def clean_last():
+    global screen_text
+    if(len(screen_text) <= 1):
+        screen_text = ''
+        screen['text'] = '0'
+    else:
+        screen_text = screen_text[:-1]
+        screen['text'] = screen_text
+
+def raise_result():
+    global screen_text
+    try:
+        is_valid()
+        screen['text'] = raise_expression()
+        screen_text = ''
+    except Exception:
+        screen['text'] = 'Invalid expression'
+        screen_text = ''
+
+def raise_expression():
+    result = None
+
+def is_valid():
+    global screen_text
+    if(not screen_text[0].isnumeric() and screen_text[0] is not '+' and screen_text[0] is not '-'):
+        raise Exception()
+    elif(not screen_text[-1].isnumeric()):
+        raise Exception()
 
 def main(args):
     global janela
@@ -20,7 +59,6 @@ def main(args):
 
     ######
     global screen_text
-    screen_text = ''
     global screen
     screen = Label(janela, text='0', height=2, font='Helvetica 15 bold')
 
@@ -48,15 +86,23 @@ def main(args):
 
     #operators buttons
     addition = Button(janela, text='+', width=10, height=5)
+    addition['command'] = partial(update_screen, '+')
     subtraction = Button(janela, text='-', width=10, height=5)
+    subtraction['command'] = partial(update_screen, '-')
     multiplication = Button(janela, text='*', width=10, height=5)
+    multiplication['command'] = partial(update_screen, '*')
     division = Button(janela, text='/', width=10, height=5)
-    equal = Button(janela, text='=', width=10, height=5)
+    division['command'] = partial(update_screen, '/')
+    equal = Button(janela, text='=', width=10, height=5, command=raise_result)
     dot = Button(janela, text='.', width=10, height=5)
+    dot['command'] = partial(update_screen, '.')
+    clear = Button(janela, text='C', width=10, height=4, command=clean_screen)
+    erase = Button(janela, text='<==', width=10, height=4, command=clean_last)
+    
 
     ######
 
-    screen.grid(row=0, column=1, columnspan=4, sticky='e')
+    screen.grid(row=0, column=3, columnspan=2, sticky='e')
 
     #gridding the numbers
     num0.grid(row=4, column=1)
@@ -71,17 +117,19 @@ def main(args):
     num9.grid(row=1, column=3)
 
     #gridding the operators
-    addition.grid(row=4, column= 2)
-    subtraction.grid(row=4, column=3)
+    addition.grid(row=4, column= 4)
+    subtraction.grid(row=3, column=4)
     multiplication.grid(row=2, column= 4)
     division.grid(row=1, column=4)
-    equal.grid(row=4, column= 4)
-    dot.grid(row=3, column=4)
+    equal.grid(row=4, column= 3)
+    dot.grid(row=4, column=2)
+    clear.grid(row=0, column=1)
+    erase.grid(row=0, column=2)
 
     ######
 
     janela.title('Calculator')
-    janela.geometry('450x400')
+    janela.geometry('450x400+200+100')
     janela.mainloop()
 
 if __name__=='__main__':
