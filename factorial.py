d9d96594bbfeb4eab9e30a5fb8627a2cdc3ce455
gabrielfernandessilva <gabriel.fernandes.silva@ccc.ufcg.edu.br>
2017-04-22 19:48:40 -0300

    factorial added

---
+++ factorial.py
@@ -0,0 +1,25 @@
+from sys import argv
+
+'''
+a simple python module with
+exclusive the factorial implementetion
+
+@Author Gabriel Fernandes
+'''
+
+def factorial(num):
+    if(num < 0):
+        raise ValueError('must be a natural number')
+    
+    if(not num <= 1):
+        num = num * factorial(num - 1)
+
+    return 1 if num is 0 else num
+
+
+if(__name__=='__main__'):
+    try:
+        print(factorial(int(argv[1])))
+        
+    except(IndexError, ValueError) as e:
+        print(str(type(e))[8:-2], ': Please, insert a natural number after "factorial.py" in command line', sep='')
