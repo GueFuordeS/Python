6f4c2404cd09a00e65174bbeaf2130695c1e5408
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-07-30 02:47:26 -0300

    minorafter 10/10

--- quicksortmedian.py
+++ quicksortmedian.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python3
 # Easy peasy lemon squeezy
 # My implementation of the quicksort(with median to find a best pivot)
 # Gabriel Fernandes
@@ -24,13 +25,16 @@ def sort(array, lbound=0, rbound=None):
 
 def median(array, lbound, rbound):
     lesser = lbound
+    ocurr = 1
     for i in range(lbound + 1, rbound + 1):
-        if array[i] < array[lesser]:
+        if array[i] == array[lesser]:
+            ocurr += 1
+        elif array[i] < array[lesser]:
             lesser = i
+            ocurr = 1
 
     middle = (lbound + rbound) // 2
     index = lbound
-    ocurr = 0
     med = lesser
     while index + ocurr < middle:
         minor = minorafter(array, med, lbound, rbound)
@@ -45,17 +49,21 @@ def median(array, lbound, rbound):
 def minorafter(array, index, lbound, rbound):
     minor = None
     ocurr = 0
-    for i in range(lbound, rbound + 1):
-        if array[i] == array[index] and i != index:
-            if minor == None:
-                minor = i
+    i = lbound
+    while minor == None and i <= rbound:
+        if array[index] < array[i]:
+            minor = i
             ocurr += 1
-        elif array[i] > array[index] and i != index:
-            if minor == None:
-                minor = i
-            elif array[i] < array[minor]:
-                minor = i
-    return minor
+        i += 1
+    while minor != None and i <= rbound:
+        if array[i] < array[minor] and array[i] > array[index]:
+            minor = i
+            ocurr = 1
+        elif array[i] == array[minor] and i != minor:
+            ocurr += 1
+        i += 1
+
+    return (minor, ocurr)
 
 
 def partition(array, lhand, rhand, rbound):
@@ -117,23 +125,42 @@ def test3():
 
 
 def minortest():
-    length = randint(0,20)
-    l = [randint(0,9) for i in range(length)]
-    for i in range(len(l)):
-        try:
-            index = minorafter(l, i, 0, len(l)-1)
-            assert l[i] <= l[index] and i != index
-        except TypeError:
-            assert l[i] == max(l) and index == None
+    l = [9,7,8,2,1,7,0,7]
+    minor = minorafter(l, 3, 0, len(l)-1)
+    assert l[minor[0]] == 7
+    assert minor[1] == l.count(7)
+
+    l = [1,2,3,4,5,6]
+    minor = minorafter(l, 4, 0, len(l)-1)
+    assert l[minor[0]] == 6
+    assert minor[1] == 1
+
+    l = [4,5,1,2,6,3]
+    minor = minorafter(l, 4, 0, len(l)-1)
+    assert minor[0] == None
+    assert minor[1] == 0
+
+    length = randint(1, 100)
+    array = [randint(1, 10) for i in range(length)]
+    array.sort()
+    elem_index = randint(0, length-1)
+    minor = minorafter(array, elem_index, 0, length-1)
+    '''
+    print(array)
+    print('elem:', array[elem_index])
+    print('minor:', array[minor[0]] if minor[0] != None else None)
+    print('ocurr:',minor[1])
+    '''
+    if minor[0] != None:
+        assert array.count(array[minor[0]]) == minor[1]
+    else:
+        assert max(array) == array[elem_index]
 
 
 if __name__ == '__main__':
-    minortest()
-    l = [3,5,1,2,4,2]
-    print(l)
-    print('median:', l[median(l,0,len(l)-1)])
-'''
+    '''
     test()
     test2()
     test3()
-'''
+    '''
+    minortest()
