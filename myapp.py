f3e0dacee78e20feb96a8876c09f0f030bcbd77b
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-08-17 22:28:50 -0300

    gunicorn

---
+++ myapp.py
@@ -0,0 +1,25 @@
+def num_sum(numbers):
+    nums = list(filter(lambda e: num_convert(e), numbers))
+    nums = list(map(lambda e: int(e), nums))
+
+    return sum(nums)
+
+
+def num_convert(chunk):
+    try:
+        int(chunk)
+        return True
+    except ValueError:
+        return False
+
+
+def app(environ, start_response):
+    total = num_sum(environ['PATH_INFO'][1:].split(','))
+    total = str(total).encode()
+
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(total)))
+    ])
+
+    return iter([total])
