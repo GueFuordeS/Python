336bfd35c09561080ca5650101b845cb7189bd2f
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-11-02 17:16:15 -0300

    solved

--- even_tree.py
+++ even_tree.py
@@ -1,12 +1,3 @@
-def yield_leaf(tree, offset):
-    for node in tree:
-        if(tree[node][1] == []):
-            if offset <= 0:
-                return node
-            else:
-                offset -= 1
-
-
 def cut_subtree(tree, leading):
     childs = tree[leading][1][:]
     for child in childs:
@@ -16,47 +7,42 @@ def cut_subtree(tree, leading):
     tree.pop(leading)
 
 
-def reach_even_tree(tree, leaf):
-    reached = False
-    node_counting = 1
-    node = leaf
-    while not reached:
-        print(node)
-        print(tree)
-        node = tree[node][0]
-        node_counting += 1
-        for child in tree[node][1]:
-            node_counting += 1
-        node_counting -= 1
-        if (node_counting % 2 == 0):
-            reached = True
-        if (node == tree[node][0] and reached != True):
-            return False
-    cut_subtree(tree, node)
-    return True
+def yield_tree_nvls(tree, max_nvl):
+    for nvl in range(max_nvl, 0, -1):
+        for node in list(tree.keys()):
+            try:
+                if tree[node][2] == nvl:
+                    yield node
+            except KeyError:
+                pass
+
+
+def subtree_size(tree, node):
+    size = 1
+    for child in tree[node][1]:
+        size += subtree_size(tree, child)
+    return size
 
 
 def main():
+    max_nvl = 2
     nodes, edges = map(lambda e: int(e), input().split())
+    tree = {n:[1, [], 1] for n in range(1, nodes+1)}
 
-    tree = {}
-    tree.update({n:[1, []] for n in range(1, nodes+1)})
-
-    for i in range(1, edges+1):
+    for _ in range(edges):
         edge = tuple(map(lambda e: int(e), input().split()))
         tree[edge[0]][0] = edge[1]
+        tree[edge[0]][2] += tree[edge[1]][2]
+        if max_nvl < tree[edge[0]][2]:
+            max_nvl = tree[edge[0]][2]
         tree[edge[1]][1].append(edge[0])
 
-    cuts_counting = -1
-    offset = 0
-    while tree != {}:
-        reached = reach_even_tree(tree, yield_leaf(tree, offset))
-        if reached:
-            offset = 0
-            cuts_counting += 1
-        else:
-            offset += 1
-    print(cuts_counting)
+    cutted_edges = -1
+    for node in yield_tree_nvls(tree, max_nvl):
+        if subtree_size(tree, tree[node][0]) % 2 == 0:
+            cut_subtree(tree, tree[node][0])
+            cutted_edges += 1
+    print(cutted_edges)
 
 
 if __name__ == '__main__':
